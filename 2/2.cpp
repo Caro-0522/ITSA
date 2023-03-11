cde5a5a6c4040d32f74ea1233fa2717c58a8a64e
Caro <4b1g0102@stust.edu.tw>
2023-03-11 13:46:55 +0800

    打完

--- 2/2.cpp
+++ 2/2.cpp
@@ -6,6 +6,47 @@ using namespace std;
 
 int main()
 {
+    int m1;
+    switch (m1) 
+    {
+        case 1:
+            cout << "Winter";
+            break;
+        case 2:
+            cout << "Winter";
+            break;
+        case 3:
+            cout << "Spring";
+            break;
+        case 4:
+            cout << "Spring";
+            break;
+        case 5:
+            cout << "Spring";
+            break;
+        case 6:
+            cout << "Summer";
+            break;
+        case 7:
+            cout << "Summer";
+            break;
+        case 8:
+            cout << "Summer";
+            break;
+        case 9:
+            cout << "Autumn";
+            break;
+        case 10:
+            cout << "Autumn";
+            break;
+        case 11:
+            cout << "Autumn";
+            break;
+        case 12:
+            cout << "Winter";
+            break;
+    }
+
     
 }
 

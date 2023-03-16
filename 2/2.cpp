ff64949ad69f72c8953f579b58455c04c66b0721
Caro <4b1g0102@stuat.edu.tw>
2023-03-16 17:16:28 +0800

    1

--- 2/2.cpp
+++ 2/2.cpp
@@ -1,21 +1,41 @@
-﻿// 2.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main()
 {
+    int coin = 0,time1 = 0,time2 = 0,time3 = 0,time4 = 0; 
+    int a = 0, b = 0, c = 0, d = 0;
+
+    cin >> time1 >> time2>> time3 >> time4;
+
+    if (time2 > 0) {
+        a = 60 - time2;
+        b = time3 - time1 - 1;
+    }
+    if (time2 == 0) {
+        b = time3 - time1;
+    }
     
+    c = a + time4;
+    d = c / 30;
+    //b = (c * 60) + a + time4;
+    if (b != 0 && b >= 4) {
+        coin = 120 + 160 + d * 60;
+        cout << coin << endl;
+        b = 0;
+    }
+    if (b != 0 && b >= 2) {
+        coin = 120 + d * 40;
+        cout << coin << endl;
+        b = 0;
+    }
+    if (b != 0 && b < 2) {
+        b = b * 60;
+        b = b + c;
+        b = b / 30;
+        coin = b * 30;
+        cout << coin << endl;
+        b = 0;
+    }  
 }
 
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案

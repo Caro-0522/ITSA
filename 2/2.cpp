fb2c2655365a79bb51312f3293bf109c1084c6e5
Caro <4b1g0102@stust.edu.tw>
2023-03-21 17:36:40 +0800

    修改變數名稱

--- 2/2.cpp
+++ 2/2.cpp
@@ -3,22 +3,18 @@ using namespace std;
 
 int main()
 {
-	int a = 0, ent = 0, c = 0, d = 0, i = 0;
+	int ent = 0, all = 0, a = 0, i = 0;
 	cin >> ent;
 	ent = ent / 3; //33
 	ent = ent * 3; //99
 	
-	c = ent; //99
+	a = ent; //99
 	ent = ent / 3;
 
 	for (i=1;i<=ent;i++) 
 	{
-		d = d + c;
-		c = c - 3;
+		all = all + a;
+		a = a - 3;
 	}
-	cout << d << endl;
-
-	
-
-
+	cout << all << endl;
 }

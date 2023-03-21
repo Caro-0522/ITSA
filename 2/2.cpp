11c6e238b0d1bb855daa3c63460678f9ff85a09b
Caro <4b1g0102@stust.edu.tw>
2023-03-21 17:31:42 +0800

    完成

--- 2/2.cpp
+++ 2/2.cpp
@@ -3,12 +3,22 @@ using namespace std;
 
 int main()
 {
-	int a = 0;
-	while (a>=0)
-	{
-		
+	int a = 0, ent = 0, c = 0, d = 0, i = 0;
+	cin >> ent;
+	ent = ent / 3; //33
+	ent = ent * 3; //99
+	
+	c = ent; //99
+	ent = ent / 3;
 
+	for (i=1;i<=ent;i++) 
+	{
+		d = d + c;
+		c = c - 3;
 	}
+	cout << d << endl;
+
+	
 
 
 }

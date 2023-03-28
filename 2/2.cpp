34d2c5363c65acde40e2c6fc4815c6cfefd78929
Caro <4b1g0102@stust.edu.tw>
2023-03-28 17:28:50 +0800

    完成

--- 2/2.cpp
+++ 2/2.cpp
@@ -3,39 +3,33 @@ using namespace std;
 
 int main()
 {
-    int coin = 0,time1 = 0,time2 = 0,time3 = 0,time4 = 0; 
-    int a = 0, b = 0, c = 0, d = 0;
+    int h1, m1, h2, m2;
+    int numh,numm,all;
+    cin >> h1 >> m1;
+    cin >> h2 >> m2;
 
-    cin >> time1 >> time2>> time3 >> time4;
+    numm = m2 - m1;
+    numh = h2 - h1;
 
-    if (time2 > 0) {
-        a = 60 - time2;
-        b = time3 - time1 - 1;
+    if (numm < 0) {
+        numh--;
+        numm = 60 + numm;
     }
-    if (time2 == 0) {
-        b = time3 - time1;
+
+    if (numh >= 4) {
+        int num1 = numh * 60 + numm;
+        num1 = num1 - 240;
+        all = (num1 / 30) * 60 + 280;
+        cout << all;
+        
     }
+
     
-    c = a + time4;
-    d = c / 30;
-    //b = (c * 60) + a + time4;
-    if (b != 0 && b >= 4) {
-        coin = 120 + 160 + d * 60;
-        cout << coin << endl;
-        b = 0;
-    }
-    if (b != 0 && b >= 2) {
-        coin = 120 + d * 40;
-        cout << coin << endl;
-        b = 0;
-    }
-    if (b != 0 && b < 2) {
-        b = b * 60;
-        b = b + c;
-        b = b / 30;
-        coin = b * 30;
-        cout << coin << endl;
-        b = 0;
-    }  
+
+
+
+
+
+
 }
 
